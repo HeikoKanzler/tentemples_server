ab2954b9727df1fc82dd9a7947316cbf34d40c4d
Heiko Kanzler <heiko@paperclipstudios.com>
2023-01-17 06:44:15 +0100

    Administration / Backend: listing tenants, tournaments and creating new, listing players and player details, basic editing of players

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,22 @@
+dmx.config({
+  "list": {
+    "query": [
+      {
+        "type": "number",
+        "name": "tournament_id"
+      }
+    ]
+  },
+  "main": {
+    "query": [
+      {
+        "type": "number",
+        "name": "tournament_id"
+      },
+      {
+        "type": "number",
+        "name": "id"
+      }
+    ]
+  }
+});
